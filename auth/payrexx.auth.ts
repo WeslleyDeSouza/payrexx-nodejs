2aeeff50a705c6b6d770f95b2cbcf4a43e13cc80
Weslley De Souza <we.desouza@hotmail.com>
2020-06-05 11:57:15 +0200

    a

---
+++ auth/payrexx.auth.ts
@@ -0,0 +1,32 @@
+const Base64 = require('crypto-js/enc-base64')
+const hmacSHA256 = require('crypto-js/hmac-sha256')
+
+
+/**
+ * Auth helper class
+ * @Weslley De Souza 2020
+ * */
+export class AuthHelper {
+
+    constructor(private _instance,private _secret){}
+
+    buildSignature(query , secret=this._secret , digTo = 'base64') {
+        return Base64.stringify(hmacSHA256(query, secret))
+    }
+
+    buildUrl      (json  , qUri = '',escape = false) {
+        for(const key in json){
+
+            qUri+= escape ? encodeURI(`${key}=${json[key]}&`) :`${key}=${json[key]}&`
+        }
+        return qUri.substr(0,qUri.length-1);
+    }
+
+    getCredential(){
+        return {
+            instance:this._instance,
+            secret:this._secret,
+        }
+    }
+
+}
