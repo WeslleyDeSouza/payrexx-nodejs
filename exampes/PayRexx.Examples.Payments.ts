369431927e12dc925ebd1a3c2fa332d98c0bde0d
Weslley De Souza <we.desouza@hotmail.com>
2020-09-21 10:29:01 +0200

    Create PayRexx.Examples.Payments.ts

---
+++ exampes/PayRexx.Examples.Payments.ts
@@ -0,0 +1,34 @@
+import {PayRexx} from "../index";
+// Testet 04.06.2020
+(async ()=>{
+    const auth = require('./credentials.json');
+
+    let PAYR = new PayRexx( auth.instance, auth.secret);
+
+    const payment = await PAYR.payment.create({
+        "title":       "Product XY-123",
+        "description": "test",
+        "psp":             1,
+        "referenceId": "test",
+        "purpose":     "test",
+        "amount":        10,
+        "vatRate" :      0,
+        "currency":     "CHF",
+        "sku":    "20.19.03.1",
+        "preAuthorization": false,
+        "reservation":      0,
+    });
+
+
+    console.log('PAYR.payment',payment[0].id)
+    console.log('-----------------------------------------');
+
+    const gw = await PAYR.payment.get(payment[0].id)
+    console.log(gw)
+    console.log('-----------------------------------------');
+
+    const result = await PAYR.payment.delete(payment[0].id)
+    console.log(result.status);
+
+
+})();
